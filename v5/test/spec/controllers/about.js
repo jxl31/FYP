c768159191c5ec4690aa67e586dca76623552cbf
jxl31 <john.lucente@mydit.ie>
2015-03-13 01:39:59 +0000

    removed all files from .gitignore

--- v5/test/spec/controllers/about.js
+++ v5/test/spec/controllers/about.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: HelpCtrl', function () {
 
   // load the controller's module
   beforeEach(module('myappApp'));
@@ -11,12 +11,13 @@ describe('Controller: AboutCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    AboutCtrl = $controller('HelpCtrl', {
       $scope: scope
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
+    console.log(scope.awesomeThings);
     expect(scope.awesomeThings.length).toBe(3);
   });
 });
---
+++ v6 - fullstack/client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('v6App')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
